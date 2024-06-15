b0a103fb3c75b4c6d0bb6c4fde5333b93ccf454e
AndreaFilippini <56972407+AndreaFilippini@users.noreply.github.com>
2024-06-15 16:36:18 +0200

    swap declarations and directives

--- src/GBA3D.c
+++ src/GBA3D.c
@@ -1,13 +1,10 @@
+#include "lib/gba_types.h"
+#include "lib/gba_keys.h"
+#include "lib/gba_regs.h"
 #include "lib/main.h"
 #include "lib/macro.h"
 #include "lib/defines.h"
 #include "lib/math.h"
-#include "lib/gba_types.h"
-#include "lib/gba_keys.h"
-#include "lib/gba_regs.h"
-
-//#include "_house.h"	//random obj
-#include "_torus.h"	//torus
 
 struct point{
 	s16 x;
@@ -67,6 +64,9 @@ int getVertexNum();
 s16 getZBuffer(struct point *p);
 void setZBuffer(struct point *p);
 void setDepthRange();
+
+//#include "_house.h"	//random obj
+#include "_torus.h"	//torus
 
 int main(){
 
